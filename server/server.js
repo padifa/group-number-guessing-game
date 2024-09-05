ace967996b7b660631d1a56e6ee75d5252d1a64e
padifa <padifa7@live.fr>
2024-09-05 15:56:26 -0500

    details area for the history of guesses for each player

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
-
+let random = Math.floor(Math.random() * 25 + 1 );
 const guesses = [];
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -15,8 +15,9 @@ app.use(express.static('server/public'));
 app.post('/guess', (req, res) => {
   console.log(req.body);
   if (Object.keys(req.body).length === 0 || 
-!req.body.player || 
-!req.body.guess) {
+!req.body.player1 || 
+!req.body.player2 || 
+!req.body.player3 ) {
   res.sendStatus(500)
 } else {
   guesses.push(req.body) 
@@ -24,9 +25,44 @@ app.post('/guess', (req, res) => {
 }
 
 })
+
 app.get('/guess/answer', (req, res) => {
+  let guessObj = guesses[guesses.length-1];
   console.log('sending answer');
-  res.send(guesses);
+  console.log('random num :', random);
+  let guesses2 = {
+    player1: 'guess',
+    player2: 'guess',
+    player3: 'guess'
+  }
+  if(guessObj.player1 > random){
+    guesses2.player1 = 'high';
+  } else if 
+    (guessObj.player1 === random){
+      guesses2.player1 = 'correct';
+
+  } else{
+    guesses2.player1 = 'low';
+  }
+  if(guessObj.player2 > random){
+    guesses2.player2 = 'high';
+  } else if 
+    (guessObj.player2 === random){
+      guesses2.player2 = 'correct';
+
+  } else{
+    guesses2.player2 = 'low';
+  }
+  if(guessObj.player3 > random){
+    guesses2.player3 = 'high';
+  } else if 
+    (guessObj.player3 === random){
+      guesses2.player3 = 'correct';
+
+  } else{
+    guesses2.player3 = 'low';
+  }
+  res.send(guesses2);
 })
 
 app.listen(PORT, () => {
