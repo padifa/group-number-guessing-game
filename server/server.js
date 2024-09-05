2bb40e129eda0f35e2cc07c6a09585fcabcfe80d
padifa <padifa7@live.fr>
2024-09-05 15:10:00 -0500

    created an array of guesses

--- server/server.js
+++ server/server.js
@@ -3,14 +3,31 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+const guesses = [];
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json());
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.post('/guess', (req, res) => {
+  console.log(req.body);
+  if (Object.keys(req.body).length === 0 || 
+!req.body.player || 
+!req.body.guess) {
+  res.sendStatus(500)
+} else {
+  guesses.push(req.body) 
+  res.sendStatus(201);
+}
 
+})
+app.get('/guess/answer', (req, res) => {
+  console.log('sending answer');
+  res.send(guesses);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
