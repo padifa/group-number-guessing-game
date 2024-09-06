ad8a5eea9617624cc2c7b4275d6e95a90accf003
padifa <padifa7@live.fr>
2024-09-06 11:14:20 -0500

    created a restart button that tells the server to select anew random number

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,34 +1,39 @@
 function onReady() {
-  console.log("JavaScript is loaded!")
-
+  console.log("JavaScript is loaded!");
 }
-function fetchAnswers (event){
-event.preventDefault();
-axios({
-  method: "POST",
-  url: "/guess",
-  data : {
-    player1 : document.getElementById("player1").value,
-    player2 : document.getElementById("player2").value,
-    player3 : document.getElementById("player3").value
-  }
-}).then(function (response){
-console.log(response)})
-.catch(function (error) {
-  console.log(error);
-  alert(`Hey it's not working, do something else` );
-})
+let num = 0;
+function fetchAnswers(event) {
+  event.preventDefault();
+  num++;
+  document.getElementById("counter").innerHTML = `
+  Total guesses: ${num}
+  `;
+  axios({
+    method: "POST",
+    url: "/guess",
+    data: {
+      player1: document.getElementById("player1").value,
+      player2: document.getElementById("player2").value,
+      player3: document.getElementById("player3").value,
+    },
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert(`Hey it's not working, do something else`);
+    });
 
-axios({
-  method: "GET",
-  url: "/guess/answer",
-})
-.then(function(response) {
-  console.log(response);
-const guessObj = response.data;
-const playerTable = document.getElementById("answers");
+  axios({
+    method: "GET",
+    url: "/guess/answer",
+  }).then(function (response) {
+    console.log(response);
+    const guessObj = response.data;
+    const playerTable = document.getElementById("answers");
 
-  playerTable.innerHTML += `
+    playerTable.innerHTML += `
   <tr>
     <td>player 1</td>
     <td>player 2</td>
@@ -39,10 +44,31 @@ const playerTable = document.getElementById("answers");
   <td>${guessObj.player2}</td>
   <td>${guessObj.player3}</td>
   </tr>
-  `
-
-
-})
-
+  `;
+    if (
+      guessObj.player1 === "correct" ||
+      guessObj.player2 === "correct" ||
+      guessObj.player3 === "correct"
+    ) {
+      const resetDiv = document.getElementById("resetButton");
+      const resetButton = document.createElement("button");
+      resetButton.textContent = "Reset Button";
+      resetButton.addEventListener("click", reset);
+      resetDiv.appendChild(resetButton);
+      document.getElementById("submit-button").setAttribute("disabled", true);
+    }
+  });
 }
-onReady()
+function reset() {
+  axios({
+    method: "GET",
+    url: "/random",
+  })
+    .then(function (response) {
+      window.location.reload();
+    })
+    .catch((error) => {
+      console.error(error, "error resetting");
+    });
+}
+onReady();
--- server/server.js
+++ server/server.js
@@ -1,71 +1,70 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
-let random = Math.floor(Math.random() * 25 + 1 );
+let random = Math.floor(Math.random() * 25 + 1);
+
 const guesses = [];
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
 // GET & POST Routes go here
-app.post('/guess', (req, res) => {
+app.post("/guess", (req, res) => {
   console.log(req.body);
-  if (Object.keys(req.body).length === 0 || 
-!req.body.player1 || 
-!req.body.player2 || 
-!req.body.player3 ) {
-  res.sendStatus(500)
-} else {
-  guesses.push(req.body) 
-  res.sendStatus(201);
-}
+  if (
+    Object.keys(req.body).length === 0 ||
+    !req.body.player1 ||
+    !req.body.player2 ||
+    !req.body.player3
+  ) {
+    res.sendStatus(500);
+  } else {
+    guesses.push(req.body);
+    res.sendStatus(201);
+  }
+});
 
-})
-
-app.get('/guess/answer', (req, res) => {
-  let guessObj = guesses[guesses.length-1];
-  console.log('sending answer');
-  console.log('random num :', random);
+app.get("/guess/answer", (req, res) => {
+  let guessObj = guesses[guesses.length - 1];
+  console.log("sending answer");
+  console.log("random num :", random);
   let guesses2 = {
-    player1: 'guess',
-    player2: 'guess',
-    player3: 'guess'
-  }
-  
-  if(guessObj.player1 > random){
-    guesses2.player1 = 'high';
-  } else if 
-    (guessObj.player1 === random){
-      guesses2.player1 = 'correct';
+    player1: "guess",
+    player2: "guess",
+    player3: "guess",
+  };
 
-  } else{
-    guesses2.player1 = 'low';
+  if (guessObj.player1 > random) {
+    guesses2.player1 = "high";
+  } else if (guessObj.player1 == random) {
+    guesses2.player1 = "correct";
+  } else {
+    guesses2.player1 = "low";
   }
-  if(guessObj.player2 > random){
-    guesses2.player2 = 'high';
-  } else if 
-    (guessObj.player2 === random){
-      guesses2.player2 = 'correct';
-
-  } else{
-    guesses2.player2 = 'low';
+  if (guessObj.player2 > random) {
+    guesses2.player2 = "high";
+  } else if (guessObj.player2 == random) {
+    guesses2.player2 = "correct";
+  } else {
+    guesses2.player2 = "low";
   }
-  if(guessObj.player3 > random){
-    guesses2.player3 = 'high';
-  } else if 
-    (guessObj.player3 === random){
-      guesses2.player3 = 'correct';
-
-  } else{
-    guesses2.player3 = 'low';
+  if (guessObj.player3 > random) {
+    guesses2.player3 = "high";
+  } else if (guessObj.player3 == random) {
+    guesses2.player3 = "correct";
+  } else {
+    guesses2.player3 = "low";
   }
   res.send(guesses2);
-})
-
+});
+app.get("/random", (req, res) => {
+  random = Math.floor(Math.random() * 25 + 1);
+  res.send("randomized");
+});
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
